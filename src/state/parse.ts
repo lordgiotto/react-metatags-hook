12f49109a3451442ec3e89da7377e694fd3cc54c
Lorenzo Zottar <lordgiotto@gmail.com>
2019-10-02 09:07:54 +0100

    Small parse changes and formatting

--- src/state/parse.ts
+++ src/state/parse.ts
@@ -1,4 +1,11 @@
-import { MetaTag, LinkTag, TagQueryKeys, MetaTagsConfig, MetaTagModel, InternalTag } from '../types'
+import {
+  MetaTag,
+  LinkTag,
+  TagQueryKeys,
+  MetaTagsConfig,
+  MetaTagModel,
+  InternalTag,
+} from '../types'
 
 const queryableKeys = {
   meta: ['charset', 'name', 'property', 'http-equiv'],
@@ -7,27 +14,27 @@ const queryableKeys = {
 
 // Helpers that, given the tag name and the tag attributes,
 // creates the interal tag rappresentation
-const createInternalTag = (tagName: 'meta' | 'link') =>
-  (tagAttributes: MetaTag | LinkTag) => {
-    const tagQueryableKeys = queryableKeys[tagName] || []
-    const queryKeys = tagQueryableKeys
-      .map(queryKey =>
-        tagAttributes.hasOwnProperty(queryKey)
-          ? { key: queryKey, value: tagAttributes[queryKey] }
-          : undefined
-      )
-      .filter(Boolean) as TagQueryKeys[]
-    const fallbackQueryKeys = Object.keys(tagAttributes)
-      .map(key => ({
-        key,
-        value: tagAttributes[key],
-      })) as TagQueryKeys[]
-    return {
-      tag: tagName,
-      query: queryKeys.length ? queryKeys : fallbackQueryKeys,
-      attributes: tagAttributes,
-    }
+const createInternalTag = (tagName: 'meta' | 'link') => (
+  tagAttributes: MetaTag | LinkTag
+) => {
+  const tagQueryableKeys = queryableKeys[tagName]
+  const queryKeys = tagQueryableKeys
+    .map(queryKey =>
+      tagAttributes.hasOwnProperty(queryKey)
+        ? { key: queryKey, value: tagAttributes[queryKey] }
+        : undefined
+    )
+    .filter(Boolean) as TagQueryKeys[]
+  const fallbackQueryKeys = Object.keys(tagAttributes).map(key => ({
+    key,
+    value: tagAttributes[key],
+  })) as TagQueryKeys[]
+  return {
+    tag: tagName,
+    query: queryKeys.length ? queryKeys : fallbackQueryKeys,
+    attributes: tagAttributes,
   }
+}
 const createInternalMeta = createInternalTag('meta')
 const createInternalLink = createInternalTag('link')
 
@@ -44,20 +51,18 @@ const parseMetaConfig = ({
 }: MetaTagsConfig): MetaTagModel => {
   const parsedMetaTags = metas.map(createInternalMeta)
   const parsedLinks = links.map(createInternalLink)
-  const parsedOpenGraph = Object.keys(openGraph)
-    .map(key =>
-      createInternalMeta({
-        property: `og:${key}`,
-        content: openGraph[key],
-      })
-    )
-  const parsedTwitter = Object.keys(twitter)
-    .map(key =>
-      createInternalMeta({
-        property: `twitter:${key}`,
-        content: twitter[key],
-      })
-    )
+  const parsedOpenGraph = Object.keys(openGraph).map(key =>
+    createInternalMeta({
+      property: `og:${key}`,
+      content: openGraph[key],
+    })
+  )
+  const parsedTwitter = Object.keys(twitter).map(key =>
+    createInternalMeta({
+      property: `twitter:${key}`,
+      content: twitter[key],
+    })
+  )
   const fullTagsList = [
     !!description && {
       tag: 'meta',
@@ -78,21 +83,19 @@ const parseMetaConfig = ({
     ...parsedLinks,
     ...parsedOpenGraph,
     ...parsedTwitter,
-  ].filter((tag) =>
-    tag && tag.query && Object.keys(tag.query).length
+  ].filter(
+    tag => tag && tag.query && Object.keys(tag.query).length
   ) as InternalTag[]
-  const tags = fullTagsList
-    .reduce((acc, internalTag) => {
-      const tagQueryId =
-        internalTag.query
-          .map(({ key = '', value = '' }) => `${key}=${value}`)
-          .join('~')
-      const tagId = `${internalTag.tag}_${tagQueryId}`
-      return {
-        ...acc,
-        [tagId]: internalTag,
-      }
-    }, {})
+  const tags = fullTagsList.reduce((acc, internalTag) => {
+    const tagQueryId = internalTag.query
+      .map(({ key = '', value = '' }) => `${key}=${value}`)
+      .join('~')
+    const tagId = `${internalTag.tag}_${tagQueryId}`
+    return {
+      ...acc,
+      [tagId]: internalTag,
+    }
+  }, {})
   return {
     title,
     lang,
