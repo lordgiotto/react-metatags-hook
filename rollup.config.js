75674287fcbe39add9998274f61bb48fb8457a50
Lorenzo Zottar <lordgiotto@gmail.com>
2019-10-09 00:57:44 +0100

    Create only cjs bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,17 @@
-import path from 'path'
-import typescript from 'typescript'
-import clearPlugin from 'rollup-plugin-clear'
-import jsonPlugin from 'rollup-plugin-json'
-import typescriptPlugin from 'rollup-plugin-typescript2'
-import resolvePlugin from 'rollup-plugin-node-resolve'
-import commonjsPlugin from 'rollup-plugin-commonjs'
-import sourceMapsPlugin from 'rollup-plugin-sourcemaps'
-import { terser as terserPlugin } from 'rollup-plugin-terser'
+import path from 'path';
+import typescript from 'typescript';
+import clearPlugin from 'rollup-plugin-clear';
+import jsonPlugin from 'rollup-plugin-json';
+import typescriptPlugin from 'rollup-plugin-typescript2';
+import resolvePlugin from 'rollup-plugin-node-resolve';
+import commonjsPlugin from 'rollup-plugin-commonjs';
+import sourceMapsPlugin from 'rollup-plugin-sourcemaps';
+import { terser as terserPlugin } from 'rollup-plugin-terser';
 
-import pkg from './package.json'
+import pkg from './package.json';
 
-const isProd = process.env.BUILD === 'production'
-const distFolders = Array.from(new Set([
-  path.dirname(pkg.main),
-  path.dirname(pkg.module)
-]))
+const isProd = process.env.BUILD === 'production';
+const distFolders = Array.from(new Set([path.dirname(pkg.main)]));
 
 export default {
   input: 'src/index.ts',
@@ -23,41 +20,33 @@ export default {
       file: pkg.main,
       format: 'cjs',
       sourcemap: true,
-      exports: 'named'
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true,
-      exports: 'named'
+      exports: 'named',
     },
   ],
-  external: [
-    ...Object.keys(pkg.peerDependencies || {})
-  ],
+  external: [...Object.keys(pkg.peerDependencies || {})],
   plugins: [
     clearPlugin({
-      targets: distFolders
+      targets: distFolders,
     }),
     jsonPlugin(),
     resolvePlugin({
-      browser: true
+      browser: true,
     }),
     commonjsPlugin({
-      rollupCommonJSResolveHack: true
+      rollupCommonJSResolveHack: true,
     }),
     typescriptPlugin({
       typescript,
       tsconfigOverride: !isProd
         ? {
-          compilerOptions: {
-            noUnusedLocals: false,
-            noUnusedParameters: false,
+            compilerOptions: {
+              noUnusedLocals: false,
+              noUnusedParameters: false,
+            },
           }
-        }
-        : undefined
+        : undefined,
     }),
     sourceMapsPlugin(),
     isProd && terserPlugin(),
   ].filter(Boolean),
-}
+};
