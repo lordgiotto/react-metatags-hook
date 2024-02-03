d30d95244c71bfcbc02d96c31eed60918f0f2264
Lorenzo Zottar <lordgiotto@gmail.com>
2024-02-03 19:21:10 +0100

    Small jest and eslint fixes

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
-const { TextEncoder } = require('util');
-
 module.exports = {
   roots: ['<rootDir>/src'],
   testEnvironment: '<rootDir>/jest.env.config.js',
-  globals: { TextEncoder: TextEncoder },
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
